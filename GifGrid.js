597ebb584b4fff805d0450616324965aa3fe7b3b
KenshyST <88382865+KenshyST@users.noreply.github.com>
2022-08-25 20:02:15 -0500

    Add files via upload

--- GifGrid.js
+++ GifGrid.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
 
 export const GifGrid = ({category}) => {
-    console.log(category)
-
+    useEffect( () => {
+        console.log(category)
+    }, []) // En este caso como esta vacio se genera cuando se ejecuta una sola vez el proyecto
     return(
         <>
         <h3> {category} </h3>
